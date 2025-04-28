2438be96a3dba26f17b70b3e4c874ceba3514a5f
pavan0209 <pavanasonawane@gmail.com>
2025-04-28 14:33:59 +0530

    added app_icon, splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:dream11_clone/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+import 'package:dream11_clone/app.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const App());
+  FlutterNativeSplash.remove();
 }
--- lib/styles/color.dart
+++ lib/styles/color.dart
@@ -6,7 +6,7 @@ class AppColors {
   static Color black = const Color(0xFF000000);
   static Color transparent = const Color(0x00000000);
 
-  static Color red = const Color(0xFFFF0000);
+  static Color red = const Color(0xEEE00000);
   static Color lightGrey = const Color(0xFFD3D3D3);
   static Color darkBlueGrey = const Color(0xFF455A64);
 }
